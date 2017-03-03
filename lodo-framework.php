cef2c1625b0a49a71a44d44a949275d24ba992f0
Ryan Kanner <rkanner@medianewsgroup.com>
2017-03-02 18:46:52 -0700

    framework INIT

---
+++ lodo-framework.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:     Lodo Framework
+ * Plugin URI:      http://lodothemes.com
+ * Description:     Framework for the lodothemes ecosystem
+ * Author:          Ryan Kanner
+ * Author URI:      http://rkanner.com
+ * Text Domain:     lodo-framework
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Lodo_Framework
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
